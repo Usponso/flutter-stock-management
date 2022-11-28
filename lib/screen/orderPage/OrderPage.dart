d05185323b7562627299feca28126ab8c04093ec
Adrien Bogacz <ad.bogacz@gmail.com>
2022-11-28 15:13:00 +0100

    :recycle: delete second APIcall in search bar

---
+++ lib/screen/orderPage/OrderPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stock_management/providers/CustomersProvider.dart';
+import 'package:stock_management/screen/widgets/AddButton.dart';
+import 'package:stock_management/screen/widgets/CustomersList.dart';
+import 'package:stock_management/screen/widgets/SearchInput.dart';
+import 'package:stock_management/service/customer-service.dart';
+
+import '../../modal/Customer.dart';
+
+class CustomersPage extends StatefulWidget {
+  const CustomersPage({Key? key}) : super(key: key);
+
+  @override
+  State<CustomersPage> createState() => _CustomersPageState();
+}
+
+class _CustomersPageState extends State<CustomersPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: AddButton(),
+        body: SingleChildScrollView(
+          child: Column(
+              children: [
+                  SearchInput(),
+                  CustomersList(),
+              ],
+          ),
+        )
+    );
+  }
+}
--- lib/screen/widgets/SearchInput.dart
+++ lib/screen/widgets/SearchInput.dart
@@ -10,15 +10,6 @@ import '../../providers/SearchCustomersProvider.dart';
 class SearchInput extends StatefulWidget {
   const SearchInput({Key? key}) : super(key: key);
 
-
-  Future<List<Customer>> getCustomers(BuildContext context) async {
-    return await Provider.of<CustomerProvider>(context, listen: false).getCustomers();
-  }
-
-  /*Future<List<Customer>> getCustomersSearchList(BuildContext context) async {
-    return await Provider.of<SearchCustomerProvider>(context, listen: false).getCustomersSearchList();
-  }*/
-
   @override
   State<SearchInput> createState() => _SearchInputState();
 }
@@ -27,77 +18,29 @@ class _SearchInputState extends State<SearchInput> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.symmetric(vertical: 20) ,
-        width: 300,
-        child: Form(
-            /*key: _formKey,*/
-            child: FutureBuilder<List<Customer>>(
-                future: widget.getCustomers(context),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<Customer>> snapshot) {
-                  if (snapshot.hasData) {
-                    return Container(
-                        width: MediaQuery.of(context).size.width,
-                        child: Consumer<CustomerProvider> (builder: (context, value, child) {
-                          return Card(
-                            child: ListTile(
-                              leading: Icon(Icons.search),
-                              //value: fav.searchExpression
-                              title: TextField(
-                                onChanged: (value) {
-                                  Provider.of<CustomerProvider>(context, listen: false).changeSearchExpression(value);
-                                },
-                                decoration: InputDecoration(
-                                    hintText: 'Rechercher...', border: InputBorder.none),
-                              ),
-                            ),
-                          );
-                        },)
-                    );
-
-                    /*return Consumer2<CustomerProvider, SearchCustomerProvider>(builder:
-                        (BuildContext context, customerProvider, searchProvider, Widget? child) {
-                      return SearchField(
-                        onSuggestionTap: (suggestion) {
-                          customerProvider.changeCustomersList(suggestion.searchKey);
-                        },
-                        suggestions: searchProvider.customersSearchList
-                            .map((customer) =>
-                                SearchFieldListItem(customer.companyName))
-                            .toList(),
-                        *//*suggestionState: Suggestion.expand,
-                        textInputAction: TextInputAction.next,*//*
-                        hint: 'Rechercher...',
-                        hasOverlay: false,
-                        searchStyle: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black.withOpacity(0.8),
-                        ),
-                        validator: (x) {
-                          if (!searchProvider.customersSearchList.contains(x) || x!.isEmpty) {
-                            return 'Please Enter a valid State';
-                          }
-                          return null;
-                        },
-                        searchInputDecoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black.withOpacity(0.8),
-                            ),
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red),
-                          ),
-                        ),
-                        maxSuggestionsInViewPort: 6,
-                        itemHeight: 50,
-                      );
-                    });*/
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                })));
+        margin: EdgeInsets.symmetric(vertical: 20),
+        child: Form(child:
+        Container(
+            width: MediaQuery.of(context).size.width,
+            child: Consumer<CustomerProvider>(
+              builder: (context, value, child) {
+                return Card(
+                  child: ListTile(
+                    leading: Icon(Icons.search),
+                    //value: fav.searchExpression
+                    title: TextField(
+                      onChanged: (value) {
+                        Provider.of<CustomerProvider>(context, listen: false)
+                            .changeSearchExpression(value);
+                      },
+                      decoration: InputDecoration(
+                          hintText: 'Rechercher...',
+                          border: InputBorder.none),
+                    ),
+                  ),
+                );
+              },
+            ))
+        ));
   }
 }
