2a23b75476648294812347a6f32fb9c3c433d6ce
Enzo Averlant <enaverlant@gmail.com>
2022-12-01 15:58:55 +0100

    :sparkles: feat: Adding google connection and image picker/upload

---
+++ lib/screen/connectionPage/SignInScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConnectionPage extends StatefulWidget {
+  const ConnectionPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConnectionPage> createState() => _ConnectionPageState();
+}
+
+class _ConnectionPageState extends State<ConnectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
